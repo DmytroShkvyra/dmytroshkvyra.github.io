ee130190f056814959227ddeb46437c6503c88a3
DmytroShkvyra <dshkvyra@gmail.com>
2016-03-05 00:55:23 +0200

    Change filter

--- js/dtmf_decoder.js
+++ js/dtmf_decoder.js
@@ -52,8 +52,8 @@ function success(e) {
     var outputElement = document.querySelector('#output');
     var dtmf = new DTMF({
 	sampleRate: context.sampleRate,
-	peakFilterSensitivity: 1.4,
-	repeatMin: 7,
+	peakFilterSensitivity: 1.1,
+	repeatMin: 4,
 	downsampleRate: 1,
 	threshold: 0.005
     });
