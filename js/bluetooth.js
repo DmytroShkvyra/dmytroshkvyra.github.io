62512e6ea0e081fee22c441894876728f5399fda
DmytroShkvyra <dshkvyra@gmail.com>
2016-03-04 14:06:38 +0200

    add script

---
+++ js/bluetooth.js
@@ -0,0 +1,50 @@
+function connectDevice(device) {
+  return device.connectGATT()
+  .then(...)
+}
+   
+// Simple
+        
+permissions.query({ name: 'bluetooth' })
+.then(permission => {
+  if (permission.state != 'granted') {
+    console.log('User has forbidden websites to use bluetooth...');
+    return;
+  }
+  if (permission.referringDevice) {
+    // Let's try to connect to the Physical Web object first. 
+    return connectDevice(permission.referringDevice);
+  }
+  if (permission.devices.length != 0) {
+    // Some Bluetooth devices are already allowed to interact with this website.
+    // Let's try to connect to all of them.
+    return Promise.all(permission.devices.map(device => connectDevice));
+  }
+  // Prompt user to pick a nearby hearth rate sensor. 
+  return permissions.request({ name: 'bluetooth', filters: [{ services: ['heart_rate'] }] })
+  .then(result => {
+    if (result.state != 'granted') {
+      return;
+    }
+    // Let's try to connect to the heart rate sensor.
+    return connectDevice(result.devices[0]);
+  });
+});
+
+// Restore
+
+permissions.query({ name: 'bluetooth', deviceId: '123' })
+.then(permission => {
+  if (permission.state != 'granted') {
+    console.log('User has forbidden websites to use bluetooth...');
+    return;
+  }
+  if (permission.devices.length != 0) {
+    // My previously connected Bluetooth device is still there.
+    // Let's try to connect to it.
+    return connectDevice(permission.devices[0]);
+  }
+  console.log('Previous Bluetooth device access has been revoked.');
+  // Proceed to regular workflow as above...
+});
+  
