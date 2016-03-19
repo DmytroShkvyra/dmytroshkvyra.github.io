ec3f1739af21ceb697261629e9091d9440ef3068
DmytroShkvyra <dshkvyra@gmail.com>
2016-03-20 00:05:40 +0200

    Fix

--- js/walsh.js
+++ js/walsh.js
@@ -3,11 +3,11 @@ function walsh(n, shift, step, dimension,  tresholdTimeout){
   this.n = (n !== undefined)? n: 64;
   this.shift = (shift !== undefined)? shift: 45;
   this.step = (step !== undefined)? step: 3;
-  this.dimension = (dimension !== undefined)? dimension : 2;
+  this.dimension = (dimension !== undefined)? dimension : 1;
   this.tresholdTimeout = (tresholdTimeout !== undefined)? tresholdTimeout: 200;
   this.maxCodeLength = ((Math.pow(this.n/this.dimension, this.dimension)-1).toString(2)).length;
   this.dimesionLength = this.maxCodeLength/this.dimension|0;
-  this.maxSigma = 2.8
+  this.maxSigma = 3.9
 
   
   walsh.prototype.getCodes = function(code){
